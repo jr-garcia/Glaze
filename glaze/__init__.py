8fb38a97fa8c4b5ac7b8111f8bd4c12e0ea10ba3
Javier R. García <jr-garcia@users.noreply.github.com>
2016-07-10 01:04:23 -0500

    Add path retrieving functions for foreign setup scripts.

---
+++ glaze/__init__.py
@@ -0,0 +1,11 @@
+import os
+
+
+def getGladIncludePath():
+    gladPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'glad')
+    return gladPath
+
+
+def getGladSourcePath():
+    return os.path.join(getGladIncludePath(), 'glad.c')
+
